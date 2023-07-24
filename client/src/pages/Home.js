d7201e1f97164fb072d09203a2f1998349b268ee
MarcosYH <marcosdeimos18@gmail.com>
2023-07-24 15:06:43 +0200

    add: Homepage

---
+++ client/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Home = () => {
+  return (
+    <div className='mb-20'>
+<h1 className=' sm:text-4xl text-center font-bold text-2xl my-5'> Hey !👋 welcome to my blog</h1>
+<p className='mx-auto leading-relaxed text-base m-4 ml-4'>Labore culpa ad eu ea. Sint ullamco consequat nostrud aliqua ad. Et aliqua ipsum sint eu occaecat ullamco elit mollit cillum est minim nostrud aliquip.</p>
+
+    </div>
+  )
+}
